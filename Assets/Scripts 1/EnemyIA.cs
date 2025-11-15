ac5ddc66cf5dfe832414cc8b13382ff128650f36
Khayarov Dmitry <khayarovde@gmail.com>
2025-11-15 18:46:27 +0500

    All changes

---
+++ Assets/Scripts 1/EnemyIA.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AdvancedEnemyAI : MonoBehaviour
+{
+    // Старое поведение сохраняется
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    [SerializeField] private Animator m_Animator;
+    [SerializeField] private float speedWalk = 6f;
+    [SerializeField] private float speedRun = 9f;
+
+    // Новое поведение патрулирования
+    [SerializeField] private Transform[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    // Параметры поля зрения
+    [SerializeField] private float viewRadius = 15f;
+    [SerializeField] private float viewAngle = 90f;
+    [SerializeField] private LayerMask playerMask; // Только маска игрока остаётся активной
+
+    // Текущие состояния
+    private bool isPatrolling = true;
+    private bool isChasing = false;
+    private bool caughtPlayer = false;
+
+    // Остальные поля сохранятся
+    private Transform player;
+    private Vector3 playerLastPosition = Vector3.zero;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        m_Animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player")?.transform;
+
+        // Начнём патрулирование
+        GoToNextWaypoint();
+    }
+
+    void Update()
+    {
+        // Основная логика патрулирования и взаимодействия с игроком
+        if (isPatrolling)
+        {
+            if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+            {
+                GoToNextWaypoint();
+            }
+        }
+
+        // Проверка игрока в поле зрения
+        CheckForPlayer();
+
+        // Установим параметры анимации
+        m_Animator.SetFloat("Speed", navMeshAgent.velocity.magnitude);
+        m_Animator.SetBool("isChasing", isChasing);
+    }
+
+    void CheckForPlayer()
+    {
+        if (player == null)
+            return;
+
+        float distToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distToPlayer <= viewRadius && InSightCone(player.position))
+        {
+            StartChasing();
+        }
+    }
+
+    bool InSightCone(Vector3 targetPos)
+    {
+        Vector3 dirToTarget = (targetPos - transform.position).normalized;
+        float angle = Vector3.Angle(transform.forward, dirToTarget);
+        return angle <= viewAngle / 2;
+    }
+
+    void StartChasing()
+    {
+        isPatrolling = false;
+        isChasing = true;
+        navMeshAgent.speed = speedRun;
+        navMeshAgent.SetDestination(player.position);
+    }
+
+    void StopChasing()
+    {
+        isPatrolling = true;
+        isChasing = false;
+        navMeshAgent.speed = speedWalk;
+        GoToNextWaypoint();
+    }
+
+    void GoToNextWaypoint()
+    {
+        currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+        navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+    }
+
+    // Показ поля зрения в окне Scene View
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, viewRadius);
+
+        Quaternion leftRotation = Quaternion.Euler(0, -viewAngle / 2, 0);
+        Quaternion rightRotation = Quaternion.Euler(0, viewAngle / 2, 0);
+
+        Vector3 leftDir = leftRotation * transform.forward;
+        Vector3 rightDir = rightRotation * transform.forward;
+
+        Gizmos.DrawRay(transform.position, leftDir * viewRadius);
+        Gizmos.DrawRay(transform.position, rightDir * viewRadius);
+    }
+}
---
+++ Assets/Scripts 1/PlayerMovement.cs
@@ -0,0 +1,177 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField]
+    private CharacterController controller;
+    [SerializeField]
+    private float playerSpeed = 7.0f;       //  Speed of the player movement
+    [SerializeField]
+    private float jumpForce = 10.0f;        //  Force of the player jumping
+    [SerializeField]
+    private float stepDown = 30.0f;         //  Force applied to the player when falling without jumping
+    [SerializeField]
+    [Tooltip("Fall velocity when the player is on a slope")]
+    private float fallSlopeVelocity = 8f;       //  Slope fall velocity affected too by the gravity
+    [SerializeField]
+    [Tooltip("Force in the Y-axis when the player in on a slope")]
+    private float fallSlopeForce = 10.0f;       //  Force that affects in the fall of the player in the slopes
+
+    //  Status variables accessible from other objects
+    //  Movement variables
+    private Vector3 m_PlayerMovement;   //  Movement Vector
+    private Vector3 m_Movement;         //  Input movement Vector
+    private float m_Speed;              //  Speed of the player, this changes if the player run
+    private float m_Gravity = 20.0f;    //  Gravity
+    private float m_FallVelocity;       //  Variable that saves and applies gravity to the player
+    private bool m_IsJump;              //  Control if the player has jumped
+    //  Slope variables
+    private bool m_OnSlope = false;     //  If the player is on a slope
+    private Vector3 m_HitNormal;        //  Get the normal of a plane
+
+    private Animator m_Animator;  
+
+    public bool safeZone { get; set; }
+    public bool IsRunning { get; set; }
+    public bool IsWalking { get; set; }
+
+    private void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Speed = playerSpeed;
+    }
+
+    private void Update()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");  //  Get the input axis (w,a,s,d)
+        float verticalMove = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontalMove, 0f, verticalMove);   //  Set the movement vector with the input
+        m_Movement = Vector3.ClampMagnitude(m_Movement, 1f);
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontalMove, 0f); //  Approximates and equalizes the input value if it is zero
+        bool hasVerticalInput = !Mathf.Approximately(verticalMove, 0f);
+        bool isMove = hasHorizontalInput || hasVerticalInput;   //  Determine whether or not the player is movement
+
+        m_PlayerMovement = m_Movement * m_Speed;    //  Set the player movement vector multiply the movement vector and the currently speed of the player
+
+        controller.transform.LookAt(controller.transform.position + m_Movement);//  Set the rotation of the player looking at the input vector
+
+        Gravity();
+        Jump();
+
+        if (controller.isGrounded && !m_IsJump)
+        {
+            m_PlayerMovement = m_PlayerMovement + Vector3.down * stepDown;  //  Apply more force when the player fall
+        }
+
+        controller.Move(m_PlayerMovement * Time.deltaTime);                 //  Move the player using the character controller
+        m_IsJump = !controller.isGrounded;
+
+        if (isMove)
+        {
+            //  If the player is movement, then do an action
+            Action();
+        }
+        else
+        {
+            m_Animator.SetFloat("Speed", 0);
+        }
+    }
+
+    private void OnAnimatorMove()
+    {        
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        m_HitNormal = hit.normal;   //  Take the normal of the plane where the player is walking
+    }
+
+    private void Action()
+    {
+        /*
+         *  Determine if the player is walking, running, etc
+         *  Play the animations for each action
+         */
+        if (Input.GetButton("Sprint"))
+        {
+            m_Speed = playerSpeed * 1.5f;   //  Multiply the initial speed to be able to run, if the speed change the blendtree of the animation have to change as well
+            IsWalking = false;
+            IsRunning = true;
+            m_Animator.SetFloat("Speed", controller.velocity.magnitude);    //  The animator controller take the speed
+        }
+        else
+        {
+            m_Speed = playerSpeed;
+            IsRunning = false;
+            IsWalking = true;
+            m_Animator.SetFloat("Speed", controller.velocity.magnitude);
+        }
+    }
+
+    private void Gravity()
+    {
+        /*
+         *  Gravity applies to the player
+         *  Take the gravity variable nad it is apply when the player is on the ground or when the player jumps
+         * */
+        if (controller.isGrounded)
+        {
+            m_FallVelocity = -m_Gravity * Time.deltaTime;
+            m_PlayerMovement.y = m_FallVelocity;
+        }
+        else
+        {
+            m_FallVelocity -= m_Gravity * Time.deltaTime;
+            m_PlayerMovement.y = m_FallVelocity;
+            m_Animator.SetFloat("AirSpeed", controller.velocity.y);
+            //  control if the player is falling for a long time
+            if(controller.velocity.y <= -25 || controller.velocity.y >= 20)
+            {
+                m_Animator.SetTrigger("Jump");
+            }
+        }
+        m_Animator.SetBool("IsGrounded", controller.isGrounded);
+        FallSlope();
+    }
+
+    private void Jump()
+    {
+        /*
+         *  When the player press the space bar the jump force is apply to the player
+         * */
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            m_IsJump = true;
+            m_FallVelocity = jumpForce;                 //  Set the fall velocity when the player jump with the jump force to be able to the player goes up
+            m_PlayerMovement.y = m_FallVelocity;
+            m_Animator.SetTrigger("Jump");              //  Set the trigger in the animator to jump
+        }
+    }
+
+    private void FallSlope()
+    {
+        /*
+         *  When the player is in a slope the player goes down,
+         *  calculating the angle between the Y-axis and the normal slope and 
+         *  comparing it with the slope limit of the character's controller to know 
+         *  if the player has to fall down the slope that he cannot climb
+         *  and compare if the slope is a platform with a 90 degrees plane to nor allow
+         *  the player to fall down from that slope
+         * */
+        m_OnSlope = Vector3.Angle(m_HitNormal, Vector3.up) > controller.slopeLimit && Vector3.Angle(m_HitNormal, Vector3.up) < 89;
+
+        if (m_OnSlope)
+        {
+            /*
+             * Set the player position applying a the fall velocity to every X-Z axis calculating the inclination of the slope
+             * And apply a force on the Y-axis to avoid bouncing
+             * */
+            m_PlayerMovement.x += ((1f - m_HitNormal.y) * m_HitNormal.x) * fallSlopeVelocity;
+            m_PlayerMovement.z += ((1f - m_HitNormal.y) * m_HitNormal.z) * fallSlopeVelocity;
+            m_PlayerMovement.y -= fallSlopeForce;
+        }
+    }
+}
